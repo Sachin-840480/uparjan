24fdeefa557379528745811025eb322ebd990e15
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-06-23 15:29:34 +0530

    axiosClient updated

--- uparjan_frontend/src/app/core/api/axiosClient.js
+++ uparjan_frontend/src/app/core/api/axiosClient.js
@@ -1,4 +1,7 @@
-// this file is used to create an instance of axios with a base URL for making API requests. The base URL is set to "https://localhost:5001/api", which means that all requests made using this axios instance will be prefixed with this URL. This allows for easier management of API endpoints and ensures that all requests are directed to the correct server.
+// this file is used to create an instance of axios with a base URL for making API requests. 
+//The base URL is set to "https://localhost:5001/api", which means that all requests made using this 
+// axios instance will be prefixed with this URL. This allows for easier management of API endpoints and 
+// ensures that all requests are directed to the correct server.
 
 import axios from "axios";
 
