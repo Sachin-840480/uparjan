414f3b669a3a6deb4d61a63944830d31411ead3c
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-06-23 15:17:52 +0530

    Describe your changes app.jsx is now resolved

--- uparjan_frontend/src/App.js
+++ uparjan_frontend/src/App.js
@@ -1,66 +1,33 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from
-"./dashboard/pages/Dashboard";
+import Dashboard from "./dashboard/pages/Dashboard";
 
-import FarmerRegistration from
-"./modules/farmer/pages/FarmerRegistration";
+import FarmerRegistration from "./modules/farmer/pages/FarmerRegistration";
 
-import FarmerLogin from
-"./auth/pages/FarmerLogin";
+import FarmerLogin from "./auth/pages/FarmerLogin";
 
-import OfficerLogin from
-"./auth/pages/OfficerLogin";
+import OfficerLogin from "./auth/pages/OfficerLogin";
 
-import MspLogin from
-"./auth/pages/MspLogin";
+import MspLogin from "./auth/pages/MspLogin";
 
-import MillerLogin from
-"./auth/pages/MillerLogin";
+import MillerLogin from "./auth/pages/MillerLogin";
 
 function App() {
-
   return (
     <BrowserRouter>
-
       <Routes>
+        <Route path="/" element={<Dashboard />} />
 
-        <Route
-          path="/"
-          element={<Dashboard />}
-        />
+        <Route path="/farmer-registration" element={<FarmerRegistration />} />
 
-        <Route
-          path="/farmer-registration"
-          element={<FarmerRegistration />}
-        />
+        <Route path="/farmer/login" element={<FarmerLogin />} />
 
-        <Route
-          path="/farmer/login"
-          element={<FarmerLogin />}
-        />
+        <Route path="/officer/login" element={<OfficerLogin />} />
 
-        <Route
-          path="/officer/login"
-          element={<OfficerLogin />}
-        />
+        <Route path="/msp/login" element={<MspLogin />} />
 
-        <Route
-          path="/msp/login"
-          element={<MspLogin />}
-        />
-
-        <Route
-          path="/miller/login"
-          element={<MillerLogin />}
-        />
-
+        <Route path="/miller/login" element={<MillerLogin />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
