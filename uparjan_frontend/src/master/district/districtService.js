94f912e6450593e31687a6eb616bb2798ff8d1af
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-06-25 16:00:43 +0530

    some file are chnaged

--- uparjan_frontend/src/master/district/districtService.js
+++ uparjan_frontend/src/master/district/districtService.js
@@ -1,5 +1,5 @@
 import axiosClient from "../../core/api/axiosClient";
-
+console.log("axiosClient =", axiosClient);
 export const getDistricts = async () => {
   return await axiosClient.get("/districts");
 };
